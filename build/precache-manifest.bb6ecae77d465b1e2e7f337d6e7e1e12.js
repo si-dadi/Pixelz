631d6c27369cc9d248876944cc193cdbd4607d0c
si-dadi <109419186+si-dadi@users.noreply.github.com>
2022-08-09 15:42:19 +0530

    Add files via upload

---
+++ build/precache-manifest.bb6ecae77d465b1e2e7f337d6e7e1e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fe47e498b20d42a5363775f16fa9b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8bacbaa53a12e4b7966",
+    "url": "/static/js/2.9a0a2471.chunk.js"
+  },
+  {
+    "revision": "bf72086b44f10c47f01e16d9fe377d01",
+    "url": "/static/js/2.9a0a2471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285fa1883f89b49385bb",
+    "url": "/static/js/main.b19d6618.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "f82069c38aa17775d6edd6184ee79a21",
+    "url": "/static/media/title.f82069c3.jpg"
+  }
+]);
